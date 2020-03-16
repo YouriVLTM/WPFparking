ea6a386d8e14e301daff233c8d154c58bd131d99
Youri Van Laer <r0694461@student.thomasmore.be>
2020-03-16 11:38:58 +0100

    Parking details

---
+++ parking/View/Parking.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace parking.View
+{
+    /// <summary>
+    /// Interaction logic for Parking.xaml
+    /// </summary>
+    public partial class Parking : Page
+    {
+        public Parking()
+        {
+            InitializeComponent();
+        }
+    }
+}
