c537a7910a8d93598bc76a4e3e63bd4e54495d90
Youri Van Laer <r0694461@student.thomasmore.be>
2020-03-30 12:24:26 +0200

    View Model ParkPlace

--- UnitTestUsers/UnitTestUsersViewModel.cs
+++ UnitTestUsers/UnitTestUsersViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using parking;
+using parking.Model;
 using parking.ViewModel;
 
 
@@ -10,12 +12,39 @@ namespace UnitTestParking
     public class UnitTestUsersViewModel
     {
         [TestMethod]
+        public void TestMethodParameters()
+        {
+            User user = new User();
+            user.Prename = "Youri";
+            user.Lastname = "Van Laer";
+            user.PhoneNumber = "014588620";
+            user.Email = "youri.vanlaer@hotmail.com";
+
+            Assert.AreEqual("Youri", user.Prename);
+            Assert.AreEqual("Van Laer", user.Lastname);
+            Assert.AreEqual("014588620", user.PhoneNumber);
+            Assert.AreEqual("youri.vanlaer@hotmail.com", user.Email);
+
+
+        }
+
+        [TestMethod]
         public void TestMethodAantalUsers()
         {
             var viewmodel = new UsersViewModel();
-
-            Assert.AreEqual(7, viewmodel.Users.Count);
+            Assert.AreEqual(8, viewmodel.Users.Count);
 
         }
+
+        [TestMethod]
+        public void TestMethodSelectUser()
+        {
+            var viewmodel = new UsersViewModel();
+            viewmodel.SelectedUser = viewmodel.Users.First();
+            Assert.AreEqual("youri", viewmodel.SelectedUser.Prename);
+
+        }
+
+        
     }
 }
---
+++ parking/ViewModel/ParkPlaceViewModel.cs
@@ -0,0 +1,133 @@
+﻿using parking.Extensions;
+using parking.Messages;
+using parking.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace parking.ViewModel
+{
+    class ParkPlaceViewModel : BaseViewModel
+    {
+
+        private ObservableCollection<ParkPlace> parkPlaces;
+
+        public ObservableCollection<ParkPlace> ParkPlaces
+        {
+            get
+            {
+                return parkPlaces;
+            }
+            set
+            {
+                parkPlaces = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ParkPlace selectedParkPlace;
+        public ParkPlace SelectedParkPlace
+        {
+            get
+            {
+                return selectedParkPlace;
+            }
+            set
+            {
+                selectedParkPlace = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        private ICommand toevoegenCommand;
+        public ICommand ToevoegenCommand
+        {
+            get
+            {
+                return toevoegenCommand;
+            }
+
+            set
+            {
+                toevoegenCommand = value;
+            }
+        }
+
+        private ICommand wijzigenCommand;
+        public ICommand WijzigenCommand
+        {
+            get
+            {
+                return wijzigenCommand;
+            }
+
+            set
+            {
+                wijzigenCommand = value;
+            }
+        }
+
+
+        private DialogService dialogService;
+
+        public ParkPlaceViewModel()
+        {
+            //laden data
+            ParkPlaceDataService ds = new ParkPlaceDataService();
+            parkPlaces = ds.GetParkPlace();
+
+            //instantiëren DialogService als singleton
+            dialogService = new DialogService();
+
+            //koppelen commands
+            WijzigenCommand = new BaseCommand(WijzigenParkPlace);
+            ToevoegenCommand = new BaseCommand(ToevoegenParkPlace);
+
+            //luisteren naar messages vanuit detailvenster
+            Messenger.Default.Register<UpdateFinishedMessage>(this, OnMessageReceived);
+
+        }
+
+        private void OnMessageReceived(UpdateFinishedMessage message)
+        {
+            //na update of delete mag detailvenster sluiten
+            dialogService.CloseDetailDialog();
+
+            //na Delete/Insert moet collectie Koffies terug ingeladen worden
+            if (message.Type != UpdateFinishedMessage.MessageType.Updated)
+            {
+                ParkPlaceDataService ds = new ParkPlaceDataService();
+                parkPlaces = ds.GetParkPlace();
+            }
+
+        }
+
+        private void ToevoegenParkPlace()
+        {
+
+            SelectedParkPlace = new ParkPlace();
+
+            Messenger.Default.Send<ParkPlace>(SelectedParkPlace);
+
+            dialogService.ShowDetailDialog();
+
+        }
+
+        private void WijzigenParkPlace()
+        {
+            if (SelectedParkPlace != null)
+            {
+                Messenger.Default.Send<ParkPlace>(SelectedParkPlace);
+
+                dialogService.ShowDetailDialog();
+            }
+        }
+
+
+    }
+}
